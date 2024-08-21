f431651758883bfeadfaee1f65a42a3c8b492789
Mohammed-Azab <mohammed3zab@gmail.com>
2024-08-21 21:06:40 +0200

    Made the CLI Controller

--- src/main/java/game/GUI/GUIController.java
+++ src/main/java/game/GUI/GUIController.java
@@ -1,9 +1,14 @@
 package game.GUI;
 
+import game.engine.CLIController;
+import javafx.fxml.Initializable;
 import javafx.scene.image.ImageView;
 import javafx.scene.shape.Rectangle;
 
-public class GUIController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class GUIController implements Initializable {
     public ImageView C, C1, C2, C3, C4, C5, C6, C7, C8;
     public Rectangle r1,r2,r3,r4,r5,r6,r7,r8,r9;
     public Rectangle r11,r12,r13,r14,r15,r16,r17,r18;
@@ -17,5 +22,10 @@ public class GUIController {
     public Rectangle r91,r92,r93,r94,r95,r96,r97,r98;
 
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        CLIController controller= new CLIController();
 
+
+    }
 }
--- src/main/java/game/engine/Main.java
+++ src/main/java/game/engine/Main.java
@@ -1,16 +1,33 @@
 package game.engine;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
-import javax.swing.*;
+import java.util.Objects;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage stage)  {
+    public void start(Stage primaryStage)  {
         try {
-
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/FXML/game.fxml"));
+            Parent root = loader.load();
+            Scene scene = new Scene(root);
+            Image icon = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/images/GameIntroAssest.png")));
+            primaryStage.getIcons().add(icon);
+            primaryStage.setTitle("Dice Realms");
+            primaryStage.setScene(scene);
+            primaryStage.setFullScreen(false);
+            primaryStage.setFullScreenExitHint("");
+            primaryStage.setFullScreenExitKeyCombination(javafx.scene .input.KeyCombination.valueOf("q"));
+            primaryStage.setResizable(false);
+            primaryStage.initStyle(StageStyle.UNDECORATED);
+            primaryStage.show();
         }
         catch (Exception e) {
             e.printStackTrace();
