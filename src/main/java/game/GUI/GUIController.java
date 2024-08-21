16933b98065730363d0b1135df738d0b38944b5f
Mohammed-Azab <mohammed3zab@gmail.com>
2024-08-21 16:28:20 +0200

    Started GUI preview

---
+++ src/main/java/game/GUI/GUIController.java
@@ -0,0 +1,21 @@
+package game.GUI;
+
+import javafx.scene.image.ImageView;
+import javafx.scene.shape.Rectangle;
+
+public class GUIController {
+    public ImageView C, C1, C2, C3, C4, C5, C6, C7, C8;
+    public Rectangle r1,r2,r3,r4,r5,r6,r7,r8,r9;
+    public Rectangle r11,r12,r13,r14,r15,r16,r17,r18;
+    public Rectangle r21,r22,r23,r24,r25,r26,r27,r28;
+    public Rectangle r31,r32,r33,r34,r35,r36,r37,r38;
+    public Rectangle r41,r42,r43,r44,r45,r46,r47,r48;
+    public Rectangle r51,r52,r53,r54,r55,r56,r57,r58;
+    public Rectangle r61,r62,r63,r64,r65,r66,r67,r68;
+    public Rectangle r71,r72,r73,r74,r75,r76,r77,r78;
+    public Rectangle r81,r82,r83,r84,r85,r86,r87,r88;
+    public Rectangle r91,r92,r93,r94,r95,r96,r97,r98;
+
+
+
+}
